f31ec15a3fa08a9d3b9519bcc1af52784978e87a
miletrij <quester@inbox.ru>
2024-05-09 00:59:08 +0400

    HomeWork 21.1 изменения

--- catalog/templatetags/my_tags.py
+++ catalog/templatetags/my_tags.py
@@ -3,8 +3,8 @@ from django import template
 register = template.Library()
 
 
-@register.filter()
-def media_filter(path):
-    if path:
-        return f"/media/{path}"
+@register.simple_tag
+def media_filter(data):
+    if data:
+        return f"/media/{data}"
     return "#"
